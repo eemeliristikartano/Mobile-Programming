cb7fb148750dc5fd89d9697201ebf783458ebcd1
Eemeli Ristikartano <eemeli.ristikartano@gmail.com>
2023-01-23 15:22:41 +0200

    done

--- simple_calculator/App.tsx
+++ simple_calculator/App.tsx
@@ -1,27 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Alert, Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 import { useState } from 'react';
 
+type THistory = {
+  numA: number
+  numB: number
+  operation: string
+  result: number
+}
+
 export default function App() {
+  const [history, setHistory] = useState<THistory[]>([]);
   const [result, setResult] = useState<number>(0);
   const [uppreInput, setUpperInput] = useState<string>('');
   const [lowerInput, setLowerInput] = useState<string>('');
 
-  // Handels addition or subtraction.
+  // Handles addition or subtraction.
   const handleMathematicalOperation = (operation: string) => {
     // If a decimal separator is a comma --> replace it with a dot. Change from a string to a number.
     const uppreInputAsNumber = Number(uppreInput.replace(',', '.'));
     const lowerInputAsNumber = Number(lowerInput.replace(',', '.'));
-    // It the numbers are valid.
-    if (!Number.isNaN(uppreInputAsNumber) && !Number.isNaN(lowerInputAsNumber)) {
+    // If the numbers are valid.
+    if (isNumbers(uppreInputAsNumber, lowerInputAsNumber)) {
+
       // If the operation is an addtion.
-      if (operation === 'addition') setResult(uppreInputAsNumber + lowerInputAsNumber);
+      if (operation === '+') {
+        setResult(uppreInputAsNumber + lowerInputAsNumber);
+        addToHistory(uppreInputAsNumber, lowerInputAsNumber, operation);
+      }
+
       // Else it is subtraction.
-      else setResult(uppreInputAsNumber - lowerInputAsNumber);
+      else {
+        setResult(uppreInputAsNumber - lowerInputAsNumber);
+        addToHistory(uppreInputAsNumber, lowerInputAsNumber);
+      }
+
+      // If the numbers are not valid.
     } else {
       // Alert user if there is a problem with the numbers.
       Alert.alert('Error', 'Input must be a number!');
     }
+  }
+
+  /*
+  * Are the numbers valid.
+  */
+  const isNumbers = (numA: number, numB: number): boolean => {
+    if (isNaN(numA) || isNaN(numB)) return false;
+    return true;
+  }
+
+  /*
+  * Add calculation to history.
+  */
+
+  const addToHistory = (numA: number, numB: number, operation?: string): void => {
+    if (operation === '+') {
+      setHistory([...history,
+      {
+        numA: numA,
+        numB: numB,
+        operation: '+',
+        result: numA + numB
+      }
+      ]);
+    } else {
+      setHistory([...history,
+      {
+        numA: numA,
+        numB: numB,
+        operation: '-',
+        result: numA - numB
+      }
+      ]);
+    }
+
   }
 
 
@@ -46,16 +99,22 @@ export default function App() {
         <View style={{ width: 40 + '%' }} >
           <Button
             title='+'
-            onPress={() => handleMathematicalOperation('addition')}
+            onPress={() => handleMathematicalOperation('+')}
           />
         </View>
         <View style={{ width: 40 + '%' }} >
           <Button
             title='-'
-            onPress={() => handleMathematicalOperation('subtraction')}
+            onPress={() => handleMathematicalOperation('-')}
           />
         </View>
       </View>
+      <Text style={{ fontSize: 25 }} >History</Text>
+      <FlatList
+        data={history}
+        renderItem={({ item, index }) => <Text style={{ fontSize: 20 }} >{`${index + 1}.    ${item.numA} ${item.operation} ${item.numB} = ${item.result}`}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -66,7 +125,8 @@ const styles = StyleSheet.create({
     flex: 2,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    marginTop: 80
   },
   textInput: {
     borderColor: 'black',
